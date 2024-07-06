a8fc71c69624a9df951b8e1f3362ba16a7f668a4
Elizabeth Mazorra Vidal <emazorra1@gmail.com>
2024-07-06 16:50:25 -0400

    adding package json and finished webpack

--- webpack.config.js
+++ webpack.config.js
@@ -11,5 +11,48 @@ module.exports = {
         publicPath: '/',
         filename: 'bundle.js',
     },
-    
-}
+    devtool: 'eval-source-map',
+    mode: 'development',
+    devServer: {
+        host: '0.0.0.0',
+        port: 8080,
+        hot: true,
+        historyApiFallback: true,
+
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+            publicPath: '/'
+        },
+
+        headers: {
+            'Access-Control-Allow-Origin': '*['
+        },
+        proxy: {
+            '/api/**': {
+                target: 'http://localhost:3000',
+                secure: false,
+            },
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /.(css|scss)$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ]
+};
